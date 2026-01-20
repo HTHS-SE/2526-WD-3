f4db77d0e878616715e7b0884ee2c301b8d293ba
Wellner-Research <185801148+Wellner-Research@users.noreply.github.com>
2026-01-20 18:59:31 -0500

    neatened main.js

--- js/main.js
+++ js/main.js
@@ -1,74 +1,106 @@
+/*
+Author: Sriyan Yarlagadda (neatened a bit by Ethan)
+file: main.js
+Purpose: properly render the  header navbar based on whether user is logged in or not
+and export the function for use in other js files that also use window.onload
+*/
+
 import {getUserName, signOutUser} from './lib.js';
-// Add onclick property to login button
 
 // Check if user is currently logged in to update HTML accordingly
 // Sriyan coded this. 
 function updateNavbar(){
+  //get current user info
   let user = getUserName();
   
   if (user === null){
+    //user is not logged in
     const navbar = document.getElementById("navbar-div");
     navbar.classList.add("navbar-expand-lg");
     navbar.classList.remove("navbar-expand-xl");
-    const flightsLink = document.getElementById("flightsLink")
-    const bookingLink = document.getElementById("bookingLink")
+
+    // remove flights and booking links if they exist
+    const flightsLink = document.getElementById("flightsLink");
+    const bookingLink = document.getElementById("bookingLink");
+
+    //check if elements exist before removing to prevent errors
     if (flightsLink){
-      flightsLink.remove()
+      flightsLink.remove();
     }
+
     if (bookingLink){
-      bookingLink.remove()
+      bookingLink.remove();
     }
+
+    //Set login button to redirect to login page
     const loginButton = document.getElementById("loginButton");
-    loginButton.innerHTML = "Log In";
+    loginButton.innerHTML = "Log In"; //Change button text
+
     loginButton.onclick = function(){
-      window.location= "login.html";
+      window.location = "login.html"; //redirect to login page when clicked
     }
+
   }
-  else {
+  
+  else { //User is logged in
+
+
+    //redirect to dashboard if on login or signup page
     if(window.location.href.includes('login') || window.location.href.includes('signup')){
       window.location = "dashboard.html";
     }
+
+
     const navbar = document.getElementById("navbar-div");
+    
+    // make sure there is only one instance of navbar expansion class
     navbar.classList.remove("navbar-expand-lg");
     navbar.classList.add("navbar-expand-xl");
+
+    //Declare navlist to add new elements
     const navlist = document.getElementById("navlist");
 
+    // add dashboard link
     const flights = document.createElement("li");
     flights.id = "flightsLink";
     flights.classList="nav-item";
-    const flightsLink = document.createElement("a");
-    flightsLink.href = "dashboard.html";
-    flightsLink.classList= "text-site-theme nav-underline nav-link hover-underline-animation nbMenuItem navbar-item";
-    flightsLink.innerHTML= "Dashboard";
-    flights.appendChild(flightsLink);
+    
+    //simplified this to just use innerHTML instead of creating.
+    flights.innerHTML= `
+      <a href="dashboard.html" class="text-site-theme nav-underline nav-link hover-underline-animation nbMenuItem navbar-item">
+        Dashboard
+      </a>`;
 
+
+    // insert before login button
     const buttonListElement = document.getElementById("loginButton").parentElement.parentElement
     navlist.insertBefore(flights, buttonListElement);
 
+    // add booking link
     const booking = document.createElement("li");
     booking.id = "bookingLink";
-    booking.classList="nav-item";
-    const bookingLink = document.createElement("a");
-    bookingLink.href = "booking.html";
-    bookingLink.classList= "text-site-theme nav-underline nav-link hover-underline-animation nbMenuItem navbar-item";
-    bookingLink.innerHTML= "Booking";
-    booking.appendChild(bookingLink);
+    booking.classList = "nav-item";
+    booking.innerHTML =  `
+      <a href="booking.html" class="text-site-theme nav-underline nav-link hover-underline-animation nbMenuItem navbar-item">
+        Booking
+      </a>`;
 
     navlist.insertBefore(booking, buttonListElement);
 
-
+    //Finally, set login button to sign out user
     const loginButton = document.getElementById("loginButton");
     loginButton.innerHTML = "Log Out";
     loginButton.onclick = function(){
-      signOutUser();
-      updateNavbar();
+      signOutUser(); //sign out user when clicked - declared in lib.js
+      updateNavbar(); // update navbar after signing out
     }
+
   }
 }
 
 window.onload = function(){
-  updateNavbar();
-  console.log("page loaded");
+  updateNavbar(); //call function
+  // console.log("page loaded");
 }
 
 export {updateNavbar};
